2ad9fa87e051c0759dbb1df7f3926ec4e08342a2
gi <85405443+giovannarevitoroz@users.noreply.github.com>
2024-07-27 16:36:08 -0300

    Add files via upload

---
+++ aplicacao/Endereco.java
@@ -0,0 +1,6 @@
+package cep.via.consulta.aplicacao;
+
+public record Endereco(String cep, String logradouro,
+                       String complemento, String bairro,
+                       String localidade, String uf) {
+}
